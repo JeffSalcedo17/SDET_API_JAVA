65bbac038fdf13a2af9e9a75a4133e5920e2f916
yefferson.salcedo <yefferson.salcedo@COL-LT-220216B.ad.unosquare.com>
2022-03-18 00:41:44 -0500

    first commit

--- src/test/java/com/unosquare/FirstAPITest.java
+++ src/test/java/com/unosquare/FirstAPITest.java
@@ -1,17 +1,9 @@
 package com.unosquare;
 
-import static org.hamcrest.Matchers.*;
 
-import org.testng.Assert;
-import org.testng.Reporter;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import static io.restassured.RestAssured.*;
 
 
 public class FirstAPITest {
