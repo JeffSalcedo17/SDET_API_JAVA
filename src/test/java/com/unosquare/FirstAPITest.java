c5ab72aa2ea218c34230ed89365d65b88febd681
yefferson.salcedo <yefferson.salcedo@COL-LT-220216B.ad.unosquare.com>
2022-03-18 00:25:10 -0500

    First conf

--- src/test/java/com/unosquare/FirstAPITest.java
+++ src/test/java/com/unosquare/FirstAPITest.java
@@ -17,51 +17,12 @@ import static io.restassured.RestAssured.*;
 public class FirstAPITest {
   @Test
   public void validateJSONBodyHTTPObjectUser() {
-	    RestAssured.baseURI = "https://reqres.in/api/";
-		RequestSpecification httpRequest = RestAssured.given();
-		Response response = httpRequest.get("/users/2");
-		
-		int statusCode = response.getStatusCode();
-
-		// Assert that correct status code is returned.
-		Assert.assertEquals(statusCode,200);	
-		Reporter.log("Sucess 200 validation");
-		
-		response.then().body("data.email", startsWith("janet.weaver"));
-		response.then().body("data.email", endsWith("reqres.in"));
-		response.then().body("data.first_name", equalTo("Janet"));
-		response.then().body("data.last_name", equalTo("Weaver"));
-		response.then().body("data.avatar", containsString("https://reqres.in/img/faces"));
-		
-		response.then().body("support.url", equalTo("https://reqres.in/#support-heading"));
-		response.then().body("support.text", equalTo("To keep ReqRes free, contributions towards server costs are appreciated!"));
-		
-		Reporter.log(response.body().asString());
   }
   
   
   @Test
   public void validateJSONBodyHTTPObjectColor() {
-	    RestAssured.baseURI = "https://reqres.in/api/";
-		RequestSpecification httpRequest = RestAssured.given();
-		Response response = httpRequest.get("/unknown/2");
-		
-		int statusCode = response.getStatusCode();
-
-		// Assert that correct status code is returned.
-		Assert.assertEquals(statusCode,200);	
-		Reporter.log("Sucess 200 validation");
-		
-		response.then().body("data.name", equalTo("fuchsia rose"));
-		response.then().body("data.year", equalTo(2001));
-		response.then().body("data.color", equalTo("#C74375"));
-		response.then().body("data.pantone_value", containsString("17-2031"));
-		
-		response.then().body("support.url", equalTo("https://reqres.in/#support-heading"));
-		response.then().body("support.text", equalTo("To keep ReqRes free, contributions towards server costs are appreciated!"));
-		
-		Reporter.log(response.body().asString());
-  }
+	      }
  
   
   @BeforeMethod
